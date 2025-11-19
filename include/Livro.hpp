a6b37eb5c1c0c3c7f41ca345515d5efda2020e23
Paulo <paulogomesmg@gmail.com>
2025-11-19 01:04:22 -0300

    coloquei somente os atributos da classe Livro

---
+++ include/Livro.hpp
@@ -0,0 +1,37 @@
+#ifndef LIVRO_HPP
+#define LIVRO_HPP
+
+#include <string>
+#include <iostream>
+
+class Livro {
+    private: 
+    std::string titulo; 
+    std::string autor; 
+    std::string  isbn; 
+    int quantidadeTotal; 
+    int quantidadeDisponivel; 
+    int quantidadeReservada;    
+    
+    public:
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
