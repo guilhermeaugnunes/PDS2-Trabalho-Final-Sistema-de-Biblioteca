d54ad9705e6ff3261e2de332866678352d6c4c82
Guilherme Nunes <guilhermeaugustonunes.2005@gmail.com>
2025-11-18 22:53:21 -0300

    Implementa TAD Biblioteca na branch usuario

--- include/Biblioteca.hpp
+++ include/Biblioteca.hpp
@@ -1,4 +1,32 @@
 #ifndef BIBLIOTECA_HPP
 #define BIBLIOTECA_HPP
 
+#include "Aluno.hpp"
+#include "Emprestimo.hpp"
+#include "Livro.hpp"
+#include "Professor.hpp"
+#include "Reserva.hpp"
+#include "SistemaNotificacao.hpp"
+#include "Usuario.hpp"
+
+#include <map>
+#include <string>
+#include <iostream>
+
+class Biblioteca {
+    private:
+    std::map<int, Usuario*> _usuarios;
+    // (Futuramente haverá _livros, _emprestimos, etc.)
+
+    public:
+    Biblioteca();
+    ~Biblioteca();
+
+    void cadastrarUsuario(Usuario* usuario);
+    void removerUsuario (int matricula);
+    Usuario* buscarUsuario (int matricula);
+    void listarUsuarios() const;
+
+};
+
 #endif
---
+++ src/Biblioteca.cpp
@@ -0,0 +1,42 @@
+#include "Biblioteca.hpp"
+#include "Usuario.hpp"
+#include <iostream>
+
+
+// ----------------Instancias da Classe Usuario - Guilherme----------------
+Biblioteca::Biblioteca() {}
+Biblioteca::~Biblioteca() {
+    for (auto const& pair : _usuarios) {
+        delete pair.second;
+    }
+    _usuarios.clear();
+}
+
+void Biblioteca::cadastrarUsuario(Usuario* usuario) {
+    if (_usuarios.count(usuario->getMatricula()) > 0) {
+        std::cout << "Erro: Matricula ja existente. " << std::endl;
+        return;
+    }
+    _usuarios[usuario->getMatricula()] = usuario;
+}
+
+void Biblioteca::removerUsuario (int matricula) {
+    auto it = _usuarios.find(matricula);
+    if (it != _usuarios.end()) {
+        Usuario* u = it->second;
+
+        if (u->verificaPendencias()) {
+            std::cout << "Erro: Ha endencias." << std::endl;
+            return;
+        }
+
+        delete u;
+        _usuarios.erase(it);
+        std::cout << "Usuario Deletado" << std::endl;
+    }
+    else {
+        std::cout << "Usuario nao encontrado. " << std::endl;
+    }
+}
+Usuario* Biblioteca::buscarUsuario (int matricula)  {}
+void Biblioteca::listarUsuarios() const  {}
