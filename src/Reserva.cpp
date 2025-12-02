a1fbc9ae79821ebf5e77d2a8fa2bfefe9737eeb7
Paulo <paulogomesmg@gmail.com>
2025-12-01 21:32:24 -0300

    Sistema da biblioteca completo

--- src/Reserva.cpp
+++ src/Reserva.cpp
@@ -1,48 +1,24 @@
 #include "Reserva.hpp"
-#include "Usuario.hpp"
-#include "Livro.hpp"
-#include <iostream>
 
-Reserva::Reserva(Usuario* u, Livro* l) 
-    : usuario(u), livro(l), status(StatusReserva::ATIVA) {
+Reserva::Reserva(Usuario* usuario, Livro* livro) 
+    : _usuario(usuario), _livro(livro), _ativa(true) {
+    _dataReserva = "HOJE"; // Em um sistema real, use ctime
 }
-Reserva::~Reserva() {
-    
-}
+
+Reserva::~Reserva() {}
+
 Usuario* Reserva::getUsuario() const {
-    return usuario;
-}
-Livro* Reserva::getLivro() const {
-    return livro;
-}
-Reserva::StatusReserva Reserva::getStatus() const {
-    return status;
-}
-void Reserva::concluirReserva() {
-    if (status == StatusReserva::ATIVA) {
-        status = StatusReserva::CONCLUIDA;
-    }
+    return _usuario;
 }
 
-void Reserva::cancelarReserva(bool porPrazo) {
-    if (status == StatusReserva::ATIVA) {
-        if (porPrazo) {
-            status = StatusReserva::CANCELADA_PRAZO;
-            std::string msg = "Sua reserva para o livro foi cancelada pois o prazo expirou.";
-            usuario->adicionaNotificacao(msg);
-        } else {
-            status = StatusReserva::CANCELADA_USUARIO;
-        }
-    }
+Livro* Reserva::getLivro() const {
+    return _livro;
 }
-void Reserva::notificarDisponibilidade() {
-    if (status == StatusReserva::ATIVA && usuario != nullptr) {
-        std::string titulo = (livro != nullptr) ? "o livro solicitado" : "um livro";
-        
-        std::string mensagem = "O livro reservado esta disponivel para retirada.";
-        usuario->adicionaNotificacao(mensagem); 
-    }
-}
+
 bool Reserva::isAtiva() const {
-    return status == StatusReserva::ATIVA;
+    return _ativa;
+}
+
+void Reserva::cancelar() {
+    _ativa = false;
 }
